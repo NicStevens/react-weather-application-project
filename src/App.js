28d905c18f45d63bc975275d0430b4db424964c7
Nicky Stevens <lnickystevens@gmail.com>
2024-08-22 14:37:18 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Hello</h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="www.shecodes.io"
           target="_blank"
           rel="noopener noreferrer"
+          className="btn btn-primary"
         >
-          Learn React
+          Test Button
         </a>
-      </header>
+      </div>
     </div>
   );
 }
