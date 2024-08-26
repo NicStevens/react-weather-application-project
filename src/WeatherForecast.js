57d2cd8d850f6fc93a7db715225becc61e1259b5
Nicky Stevens <lnickystevens@gmail.com>
2024-08-26 14:16:20 -0400

    Added one day of the forecast and some CSS

---
+++ src/WeatherForecast.js
@@ -0,0 +1,22 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="row">
+          <div className="WeatherForecast-day">Thu</div>
+          <div>
+            <WeatherIcon code="clear-sky-day" size={45} />
+          </div>
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
